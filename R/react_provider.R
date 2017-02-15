9d850b80801cc99188b40cf3c456c28dc0db5118
Stephane <sdeboudt@hotmail.com>
2017-02-15 14:59:23 +0100

    Initial Commit

---
+++ R/react_provider.R
@@ -0,0 +1,42 @@
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+
+#' Title
+#'
+#' @param file the name of a file starting from the package's working directory
+#'
+#' @return A data frame with the contents of file
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' mydata <- read_file(csv_file)
+#' }
+read_file <- function(file) {
+  return( read.csv2(file) )
+}
+
+#' Title
+#'
+#' @param data a data frame constructed from csv file
+#'
+#' @return A plot generated with ggplot
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' plot_file(mydata)
+#' }
+plot_file <- function(data) {
+  data <- as_data_frame(data)
+  ggplot(data = data, aes(x = speed, y = dist)) +
+    geom_line()
+}
